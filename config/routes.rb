c7c5ea3183a3a61a0a2c2874783fa46725541a3b
awazienommy <coawazie@gmail.com>
2019-10-13 20:07:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  #N/B Where was the resources routing scheme above written that is creating a conflict
+  #when a 'rake routes' is ran?
+  #Ans: I had the lines below which were doing the same thing
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
 end
